034ee193ee2d32333f1c0025ba0f7e57434159b9
vagrant <vagrant@precise32.(none)>
2014-02-20 17:40:25 +0000

    data lab

--- routes/project.js
+++ routes/project.js
@@ -3,8 +3,10 @@ var models = require('../models');
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
 
-  // query for the specific project and
-  // call the following callback
+ models.Project
+  .find({"_id": projectID})
+  .exec(afterQuery);
+
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -18,6 +20,22 @@ exports.addProject = function(req, res) {
 
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
+  var newProject = new models.Project({
+    "title": form_data.project_title,
+    "date": form_data.date,
+    "summary": form_data.summary,
+    "image": form_data.image_url
+  });
+  newProject.save(afterSaving);
+
+  function afterSaving(err) { // this is a callback
+    if(err) {console.log(err); res.send(500); }
+    res.redirect('/');
+    res.send();
+  }
+
+
+
 }
 
 exports.deleteProject = function(req, res) {
@@ -25,4 +43,14 @@ exports.deleteProject = function(req, res) {
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
+
+  models.Project
+    .find({"_id": projectID})
+    .remove()
+    .exec(deleteCallback);
+
+  function deleteCallback(err){
+    if(err) { console.log(err); }
+    res.send();
+  }
 }
